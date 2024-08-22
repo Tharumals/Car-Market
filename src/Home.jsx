b78ebfd85950c0e0ad88d26eb883cc05151e73b1
Tharumal Wimalarathna <stharumal95@gmail.com>
2024-08-22 21:59:39 +0530

    Header implementation

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function Home() {
-  return <div>{<Header />}</div>;
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
 }
 
 export default Home;
